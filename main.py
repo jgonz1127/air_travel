bea486bc3d6571e7ffd60799f16edc1d49fca8b3
Justin <jgonz1127@fiu.edu>
2022-08-05 00:33:21 -0400

    added python

---
+++ main.py
@@ -0,0 +1,59 @@
+import time
+import random
+
+curr_time = 0
+speed_of_time = 1
+
+class flight:
+    def __init__(self, flight_length):
+        self.flight_length = flight_length
+# EXPAND object WITH DATA BASE SUCH AS flight_id, num_passengers etc...
+
+def if_flight(flight_percent_chance):
+    list_of_guesses = []
+    number_to_guess = random.randint(1, 10) #IF guessed then flight occurs
+    while flight_percent_chance > 0: #GENERATES GUESSES
+        guesses = random.randint(1, 10)
+        list_of_guesses.append(guesses)
+        flight_percent_chance = flight_percent_chance - 1
+
+#CHECKS if guessed
+    if number_to_guess in list_of_guesses:
+        # print(number_to_guess, " was found in ", list_of_guesses)
+        return True
+    else:
+        # print("No match found ", number_to_guess, " and ", list_of_guesses)
+        return False
+
+
+def flight_occurs(curr_time):
+    print("Flight length of ", new_flight.flight_length)
+    return new_flight.flight_length + curr_time
+
+
+# days = input("How many days of flights would you like to observe: ")
+
+# STUCK TO 12 HOURS FOR TESTING
+days = 0.5 
+
+while days > 0:
+# START OF DAY
+    print(curr_time)
+    curr_time = curr_time + 1
+    time.sleep(speed_of_time)
+
+    flight_percent_chance = random.randint(0, 10) #NUMBER OF GUESSES
+    if (if_flight(flight_percent_chance)): #FLIGHT OCCURS
+        new_flight = flight(random.randint(0, 24))
+        flight_land_time = flight_occurs(curr_time)
+        flight_occured = True
+
+# FLIGHT LANDING TIME
+    if flight_occured:
+        if(curr_time == flight_land_time):
+            print("Flight landed at ", flight_land_time)
+
+
+# ENDS CYCLE
+    if ((days * 24) + 1 == curr_time): 
+        days = 0
