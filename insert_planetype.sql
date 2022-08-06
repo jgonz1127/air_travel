3b84f421214360f43a9d49fe88f88702d20b1f93
jgonz1127 <58371998+jgonz1127@users.noreply.github.com>
2022-08-05 22:58:58 -0400

    Add files via upload

---
+++ insert_planetype.sql
@@ -0,0 +1,7 @@
+insert into PLANE_TYPE (model_number, manufacturer) values ('A320', 'Airbus');
+insert into PLANE_TYPE (model_number, manufacturer) values ('E175', 'Embraer');
+insert into PLANE_TYPE (model_number, manufacturer) values ('736-700', 'Boeing');
+insert into PLANE_TYPE (model_number, manufacturer) values ('CRJ200', 'Bombadier');
+insert into PLANE_TYPE (model_number, manufacturer) values ('737-800', 'Boeing');
+insert into PLANE_TYPE (model_number, manufacturer) values ('A321', 'Airbus');
+insert into PLANE_TYPE (model_number, manufacturer) values ('757-200', 'Boeing');
